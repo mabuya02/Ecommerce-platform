03452325c6372dd6deca5a988ff0944f1fd5f59b
mabuya02 <mainamanasseh02@gmail.com>
2023-11-02 15:01:37 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+## Complete E-commerce website in  Laravel 10
+
+
+
+### Set up :
+
+1. Clone the repo and cd into it
+2. In your terminal ```composer install```
+3. Rename or copy ```.env.example``` file to ``.env``
+4. php artisan key:generate
+5. Set your database credentials in your ```.env``` file
+6. php artisan migrate
+7. php artisan db:seed
+10. run command[laravel file manager]:-  ```php artisan storage:link```
+11. Edit ```.env``` file :- remove APP_URL
+10. ```php artisan serve``` 
+11. Visit ```localhost:8000``` in your browser
+12. Visit /admin if you want to access the admin panel. Admin Email/Password: ```admin@gmail.com```/```1111```. User Email/Password: ```user@gmail.com```/```1111```
+
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $data=array(
+            array(
+                'name'=>'Admin',
+                'email'=>'admin@gmail.com',
+                'password'=>Hash::make('1111'),
+                'role'=>'admin',
+                'status'=>'active'
+            ),
+            array(
+                'name'=>'User',
+                'email'=>'user@gmail.com',
+                'password'=>Hash::make('1111'),
+                'role'=>'user',
+                'status'=>'active'
+            ),
+        );
+
+        DB::table('users')->insert($data);
+    }
+}
